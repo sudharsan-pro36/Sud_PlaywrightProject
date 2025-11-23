522211b97c111d5090f5c146529e750a87951fb8
sudharsan-pro36 <101439494+sudharsan-pro36@users.noreply.github.com>
2025-11-23 18:56:49 +0530

    Update UiBasics.spec.js

--- tests/UiBasics.spec.js
+++ tests/UiBasics.spec.js
@@ -2,9 +2,15 @@ const {test, expect}=require('@playwright/test');
 
 test('Browsercontext test', async({browser})=>
 {
+const username=page.locator('#username');
+const password=page.locator('#password');
+const loginclick=page.locator('#signInBtn');
 const context=await browser.newContext();
 const page=await context.newPage();
 await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
 console.log(await page.title());
 await expect(page).toHaveTitle("LoginPage Practise | Rahul Shetty Academy");
+username.fill("rahulshettyacademy");
+password.fill("Password");
+loginclick.click();
 });
